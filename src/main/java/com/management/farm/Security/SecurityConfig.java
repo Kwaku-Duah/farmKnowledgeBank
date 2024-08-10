490bde0aad68baa550ae97ac13eaa60a7ed8ecee
Sheikhman_Kalderon <duah229@outlook.com>
2024-08-10 13:21:50 +0000

    hotfix: spring cors

--- src/main/java/com/management/farm/Security/SecurityConfig.java
+++ src/main/java/com/management/farm/Security/SecurityConfig.java
@@ -2,15 +2,16 @@ package com.management.farm.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -18,6 +19,9 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())  
             .cors(cors -> cors.configure(http))
+             .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/api/users/**").permitAll()  
                 .anyRequest().authenticated()); 
@@ -30,16 +34,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001")
-                .allowedMethods("*") 
-                .allowedHeaders("*");
-            }
-        };
-    }
 }
