364000744fba1dbf960bec7e9048cfbe3f19386d
Sheikhman_Kalderon <duah229@outlook.com>
2024-08-10 01:03:51 +0000

    feat: add user repository

---
+++ src/main/java/com/management/farm/Repository/userRepositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.management.farm.Repository.userRepositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.management.farm.Model.userModels.User;
+
+// this handles the DAO - data access layer (interaction with the database)
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
